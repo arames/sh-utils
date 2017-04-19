d51981ceca3218b4ae9db25015c12720c3e6600b
Alexandre Rames <alexandre.rames@uop.re>
2017-04-18 17:18:49 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository containing bash scripting utilities.
---
+++ utils.sh
@@ -0,0 +1,57 @@
+COLOUR_RED="\033[0;31m"
+COLOUR_GREEN="\033[0;32m"
+COLOUR_NONE="\033[0m"
+
+ERRORS=0
+FAILED_TRIED_COMMANDS=""
+
+print_error() {
+	echo -e "${COLOUR_RED}ERROR: ${*}${COLOUR_NONE}" >&2
+}
+
+error() {
+	print_error "$@"
+	exit 1
+}
+
+safe() {
+	echo -e "${COLOUR_GREEN}${*}${COLOUR_NONE}"
+	# Use `eval` to handle commands passed as strings. This is useful for example
+	# for `safe "echo blah > /tmp/out"`.
+	if eval "$@" ; then
+		:
+	else
+		ERRORS=$((ERRORS+1))
+		error "Failed command:\n$*";
+	fi
+}
+
+try() {
+	echo -e "${COLOUR_GREEN}${*}${COLOUR_NONE}"
+	# Use `eval` to handle commands passed as strings. This is useful for example
+	# for `safe "echo blah > /tmp/out"`.
+	if eval "$@" ; then
+		:
+	else
+		ERRORS=$((ERRORS+1))
+		FAILED_TRIED_COMMANDS="${FAILED_TRIED_COMMANDS}${COLOUR_RED}${*}${COLOUR_NONE}"
+		print_error "$@"
+	fi
+}
+
+status_and_exit() {
+	if [ $ERRORS -eq 0 ]; then
+		echo -e "${COLOUR_GREEN}success${COLOUR_NONE}"
+	else
+		echo -e "${COLOUR_RED}FAILURE${COLOUR_NONE}"
+		echo -e "${COLOUR_RED}Commands failed:${COLOUR_NONE}"
+		echo -e "${FAILED_TRIED_COMMANDS}"
+	fi
+	exit "$ERRORS"
+}
+
+
+NPROC=nproc
+# Use brew coreutils `gnproc` on OSX.
+if [[ $(uname) == 'Darwin' ]]; then export NPROC=gnproc; fi
+
