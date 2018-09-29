d6d6363ad25d1c01c9ee223b6dd65f3246808f3c
Alexandre Rames <alexandre.rames@uop.re>
2018-09-28 17:46:45 -0700

    Dissociate `safe` from printing commands.

--- utils.sh
+++ utils.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 case $- in
-  *i*) BASH_UTILS_INTERACTIVE="true";;
-  *) BASH_UTILS_INTERACTIVE="false";;
+  *i*) SH_UTILS_INTERACTIVE="true";;
+  *) SH_UTILS_INTERACTIVE="false";;
 esac
 
-BASH_UTILS_SAFE_COMMAND_FAILED="false"
+SH_UTILS_SAFE_COMMAND_FAILED="false"
 
-COLOUR_RED=${COLOUR_RED:-"\\033[0;31m"}
 COLOUR_GREEN=${COLOUR_GREEN:-"\\033[0;32m"}
 COLOUR_BLUE=${COLOUR_BLUE:-"\\033[0;94m"}
+COLOUR_ORANGE=${COLOUR_ORANGE:-"\\033[0;33m"}
+COLOUR_RED=${COLOUR_RED:-"\\033[0;31m"}
 COLOUR_NONE=${COLOUR_NONE:-"\\033[0;0m"}
 
 ERRORS=${ERRORS:-0}
@@ -21,48 +22,61 @@ print_note() {
 	echo -e "${COLOUR_BLUE}NOTE: ${*}${COLOUR_NONE}" >&2
 }
 
+note() {
+	print_note "$@"
+}
+
+print_warning() {
+	# shellcheck disable=SC2039
+	echo -e "${COLOUR_ORANGE}WARNING: ${*}${COLOUR_NONE}" >&2
+}
+
+warning() {
+	print_warning "$@"
+}
+
 print_error() {
 	# shellcheck disable=SC2039
 	echo -e "${COLOUR_RED}ERROR: ${*}${COLOUR_NONE}" >&2
 }
 
-note() {
-	print_note "$@"
-}
-
 error() {
 	print_error "$@"
-	if [ "$BASH_UTILS_INTERACTIVE" = "true" ]; then return 1; fi
+	if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then return 1; fi
 	exit 1
 }
 
-safe() {
-	if [ "$BASH_UTILS_INTERACTIVE" = "false" ] && [ "$BASH_UTILS_SAFE_COMMAND_FAILED" = "true" ]; then return 1; fi
+cmd() {
 	# shellcheck disable=SC2039
 	echo -e "${COLOUR_GREEN}${*}${COLOUR_NONE}"
-	if [ "$DRY_RUN" = "true" ]; then return; fi
+	if [ "$DRY_RUN" = "true" ]; then return 0; fi
 	# Use `eval` to handle commands passed as strings. This is useful for example
 	# for `safe "echo blah > /tmp/out"`.
-	if eval "$@" ; then
-		:
-	else
-		BASH_UTILS_SAFE_COMMAND_FAILED="true"
+	eval "$@"
+}
+
+safe() {
+	if [ "$SH_UTILS_INTERACTIVE" = "false" ] && [ "$SH_UTILS_SAFE_COMMAND_FAILED" = "true" ]; then return 1; fi
+	# Use `eval` to handle commands passed as strings. This is useful for example
+	# for `safe "echo blah > /tmp/out"`.
+	"$@"
+	rc=$?
+	if [ "$rc" -ne 0 ]; then
+		SH_UTILS_SAFE_COMMAND_FAILED="true"
 		ERRORS=$((ERRORS+1))
 		FAILED_TRIED_COMMANDS="${FAILED_TRIED_COMMANDS}\\n${COLOUR_RED}${*}${COLOUR_NONE}"
 		error "Failed command:\\n$*";
 	fi
+	return "$rc"
 }
 
 try() {
-	if [ "$BASH_UTILS_INTERACTIVE" = "false" ] && [ "$BASH_UTILS_SAFE_COMMAND_FAILED" = "true" ]; then return 1; fi
-	# shellcheck disable=SC2039
-	echo -e "${COLOUR_GREEN}${*}${COLOUR_NONE}"
-	if [ "$DRY_RUN" = "true" ]; then return; fi
+	if [ "$SH_UTILS_INTERACTIVE" = "false" ] && [ "$SH_UTILS_SAFE_COMMAND_FAILED" = "true" ]; then return 1; fi
 	# Use `eval` to handle commands passed as strings. This is useful for example
 	# for `safe "echo blah > /tmp/out"`.
-	if eval "$@" ; then
-		:
-	else
+	eval "$@"
+	rc=$?
+	if [ "$rc" -ne 0 ] ; then
 		ERRORS=$((ERRORS+1))
 		FAILED_TRIED_COMMANDS="${FAILED_TRIED_COMMANDS}\\n${COLOUR_RED}${*}${COLOUR_NONE}"
 		print_error "$@"
@@ -81,7 +95,7 @@ status_and_exit() {
 		# shellcheck disable=SC2039
 		echo -e "${FAILED_TRIED_COMMANDS}"
 	fi
-	if [ "$BASH_UTILS_INTERACTIVE" = "true" ]; then return $ERRORS; fi
+	if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then return $ERRORS; fi
 	exit "$ERRORS"
 }
 
