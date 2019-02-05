7b493cbd31da0223dd74a9c5db3c0609adb63d16
Alexandre Rames <alexandre.rames@uop.re>
2019-02-05 09:53:19 -0800

    Clean utils.sh.

--- utils.sh
+++ utils.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 case $- in
-  *i*) SH_UTILS_INTERACTIVE="true";;
-  *) SH_UTILS_INTERACTIVE="false";;
+  *i*) SH_UTILS_INTERACTIVE=${SH_UTILS_INTERACTIVE:-"true"};;
+  *) SH_UTILS_INTERACTIVE=${SH_UTILS_INTERACTIVE:-"false"};;
 esac
 
 SH_UTILS_COLOUR_RED="\\e[0;31m"
@@ -16,85 +16,90 @@ SH_UTILS_ERRORS=${SH_UTILS_ERRORS:-0}
 
 
 print_note() {
-	printf "%bNOTE: %s%b\\n" "${SH_UTILS_COLOUR_BLUE}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
+  printf "%bNOTE: %s%b\\n" "${SH_UTILS_COLOUR_BLUE}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
 }
 
 note() {
-	print_note "$@"
+  print_note "$@"
 }
 
 print_warning() {
-	printf "%bWARNING: %s%b\\n" "${SH_UTILS_COLOUR_YELLOW}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
+  printf "%bWARNING: %s%b\\n" "${SH_UTILS_COLOUR_YELLOW}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
 }
 
 warning() {
-	print_warning "$@"
+  print_warning "$@"
 }
 
 print_error() {
-	printf "%bERROR: %s%b\\n" "${SH_UTILS_COLOUR_RED}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
+  printf "%bERROR: %s%b\\n" "${SH_UTILS_COLOUR_RED}" "${*}" "${SH_UTILS_COLOUR_NONE}" >&2
 }
 
 error() {
-	SH_UTILS_ERRORS=$((SH_UTILS_ERRORS+1))
-	print_error "$@"
-	if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then
-		return 1;
-	else
-		exit 1
-	fi
+  SH_UTILS_ERRORS=$((SH_UTILS_ERRORS+1))
+  print_error "$@"
+  if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then
+    return 1;
+  else
+    exit 1
+  fi
+}
+
+print_cmd() {
+  printf "%b%s%b\\n" "${SH_UTILS_COLOUR_GREEN}" "${*}" "${SH_UTILS_COLOUR_NONE}"
 }
 
 cmd() {
-	printf "%b%s%b\\n" "${SH_UTILS_COLOUR_GREEN}" "${*}" "${SH_UTILS_COLOUR_NONE}"
-	if [ "$SH_UTILS_DRY_RUN" = "true" ]; then return 0; fi
-	# Use `eval` to handle commands passed as strings. This is useful for example
-	# for `safe "echo blah > /tmp/out"`.
-	eval "$@"
+  print_cmd "$@"
+  if [ "$SH_UTILS_DRY_RUN" = "true" ]; then return 0; fi
+  # Use `eval` to handle commands passed as strings. This is useful for example
+  # for `safe "echo blah > /tmp/out"`.
+  eval "$@"
 }
 
 safe() {
-	if [ "$SH_UTILS_ERRORS" -ne 0 ]; then return 1; fi
-	# Use `eval` to handle commands passed as strings. This is useful for example
-	# for `safe "echo blah > /tmp/out"`.
-	"$@"
-	rc=$?
-	if [ "$rc" -ne 0 ]; then
-		error "Failed command: $*";
-	fi
-	return "$rc"
+  if [ "$SH_UTILS_ERRORS" -ne 0 ]; then return 1; fi
+  # Use `eval` to handle commands passed as strings. This is useful for example
+  # for `safe "echo blah > /tmp/out"`.
+  "$@"
+  rc=$?
+  if [ "$rc" -ne 0 ]; then
+    error "Failed command: $*";
+  fi
+  return "$rc"
 }
 
-status_and_exit() {
-	if [ $SH_UTILS_ERRORS -eq 0 ]; then
-		printf "%b%s%b\\n" "${SH_UTILS_COLOUR_GREEN}" "success" "${SH_UTILS_COLOUR_NONE}"
-	else
-		printf "%b%s%b\\n" "${SH_UTILS_COLOUR_RED}" "FAILURE" "${SH_UTILS_COLOUR_NONE}"
-	fi
-
-	if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then
-		return $SH_UTILS_ERRORS;
-	else
-		exit "$SH_UTILS_ERRORS"
-	fi
-}
+#status_and_exit() {
+#	if [ $SH_UTILS_ERRORS -eq 0 ]; then
+#		printf "%b%s%b\\n" "${SH_UTILS_COLOUR_GREEN}" "success" "${SH_UTILS_COLOUR_NONE}"
+#	else
+#		printf "%b%s%b\\n" "${SH_UTILS_COLOUR_RED}" "FAILURE" "${SH_UTILS_COLOUR_NONE}"
+#	fi
+#
+#	return $SH_UTILS_ERRORS;
+#	#if [ "$SH_UTILS_INTERACTIVE" = "true" ]; then
+#	#	return $SH_UTILS_ERRORS;
+#	#else
+#	#	exit "$SH_UTILS_ERRORS"
+#	#fi
+#}
 
 
 check_nargs() {
-	if [ "$#" -lt 2 ] || [ 3 -lt "$#" ]; then
-		error "Unexpected number of arguments $#, not in [2, 3]"
-	fi
-	nargs="$1"
-	min="$2"
-	if [ "$#" -lt 3 ]; then
-		max="$min"
-	else
-		max="$3"
-	fi
-	if [ "$nargs" -lt "$min" ] || [ "$max" -lt "$nargs" ]; then
-		error "Unexpected number of arguments $nargs, not in [$min, $max]"
-	fi
-	return 0
+  if [ "$#" -lt 2 ] || [ 3 -lt "$#" ]; then
+    error "Unexpected number of arguments $#, not in [2, 3]"
+  fi
+  nargs="$1"
+  min="$2"
+  if [ "$#" -lt 3 ]; then
+    max="$min"
+  else
+    max="$3"
+  fi
+  if [ "$nargs" -lt "$min" ] || [ "$max" -lt "$nargs" ]; then
+    error "Unexpected number of arguments $nargs, not in [$min, $max]"
+  fi
+  return 0
 }
 
 
